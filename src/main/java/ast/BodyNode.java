1b29e4f5a70821689b92e4c36c7310614162f2c8
Vladislav Smirnov <beluckydaf@gmail.com>
2019-11-30 21:57:24 +0300

    Body node, var declaration

--- src/main/java/ast/BodyNode.java
+++ src/main/java/ast/BodyNode.java
@@ -1,6 +1,8 @@
 package ast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BodyNode extends Node {
 
@@ -25,13 +27,49 @@ public class BodyNode extends Node {
                 actionTypes.add(ActionType.EXPRESSION);
             } else {
                 // TODO : What to do here?
-                // Vlad: throw error I think
+                // Vlad: throw an error I think
             }
         }
     }
 
     @Override
     public String generateCode() {
-        return null;
+        StringBuilder cil = new StringBuilder();
+        HashMap<String, Integer> vars = new HashMap<>();
+        // Declaring local variables
+        int declarations = 0;
+        for (int i = 0; i < actionTypes.size(); i++) {
+            vars.put(((VariableDeclNode) actions.get(i)).name, declarations);
+            if (actionTypes.get(i) == ActionType.VARIABLE_DECLARATION) declarations++;
+        }
+        if (declarations > 0) {
+            cil.append(String.format(".maxstack %d\n", declarations));
+            cil.append(".locals init (int32 a_0"); // TODO: REPLACE int32 WITH ACTUAL DATA TYPES
+            for (int i = 1; i < declarations; i++) { //               |
+                cil.append(String.format(", int32 a_%d", i)); // Here as well
+            }
+            cil.append(")\n");
+        }
+
+        // Processing actions
+        for (int i = 0; i < actions.size(); i++) {
+            Node action = actions.get(i);
+            switch (actionTypes.get(i)) {
+                case VARIABLE_DECLARATION:
+                    cil.append(action.generateCode());
+                    int varId = vars.get(((VariableDeclNode) action).name);
+                    cil.append(String.format("stloc.s %d\n", varId));
+                    break;
+                case EXPRESSION:
+                    cil.append(action.generateCode());
+                    break;
+                case STATEMENT:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return cil.toString();
     }
 }
