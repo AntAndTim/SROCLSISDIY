913f2846c42ed6d2fbfc5968a6b91c8b9784f73a
Evgeny Romanov <dev.e.romanov@gmail.com>
2019-12-06 03:45:49 +0300

    sample test code

--- src/main/java/bison/TempEntryPoint.java
+++ src/main/java/bison/TempEntryPoint.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class TempEntryPoint {
 
     public static void main(String[] args) throws IOException {
-        YYParser.Lexer lexer = new BisonLexer(TempEntryPoint.class.getClassLoader().getResourceAsStream("parserTest.o"));
+        YYParser.Lexer lexer = new BisonLexer(TempEntryPoint.class.getClassLoader().getResourceAsStream("sample.o"));
         YYParser parser = new YYParser(lexer);
         parser.setDebugLevel(10);
         parser.parse();
